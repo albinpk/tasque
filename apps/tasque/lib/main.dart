48e073c4a71914abf3a46edcd7c473dcbb138304
Albin <56157868+albinpk@users.noreply.github.com>
2025-03-23 18:24:42 +0530

    chore: integrate Firebase configuration for Android and iOS

--- apps/tasque/lib/main.dart
+++ apps/tasque/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'src/app.dart';
 import 'src/core/db/object_box.dart';
 
@@ -8,6 +10,7 @@ late final ObjectBox objectbox;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   objectbox = await ObjectBox.create();
 
